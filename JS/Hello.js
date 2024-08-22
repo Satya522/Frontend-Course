6a136c2bfffdafba88896c1ba490d99c99d81124
Satya522 <18112017satyendra@gmail.com>
2024-08-23 01:56:42 +0530

    new file

---
+++ JS/Hello.js
@@ -0,0 +1,5 @@
+console.log("I");
+console.log("");
+console.log("know");
+console.log();
+console.log("JavaScript");
---
+++ TS/Sum.ts
@@ -0,0 +1,10 @@
+// define a function to add two number
+function add(a: number, b:number): number{
+    return a + b;
+}
+
+let num1: number = 5;
+let num2: number = 7;
+// @ts-ignore
+let result = number = add(num1,num2);
+console.log(result);
